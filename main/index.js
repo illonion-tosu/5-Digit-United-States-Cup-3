ffed2466630f9172b02f59b29b8cc2f71fdc860b
illonion <zhou.lawrence.yz@gmail.com>
2024-05-17 19:47:11 +1000

    Add score logic

--- main/index.js
+++ main/index.js
@@ -15,9 +15,35 @@ const blueTeamStarsEl = document.getElementById("blueTeamStars")
 let currentRedTeamName, currentBlueTeamName
 let currentBestOf, currentFirstTo, currentRedStars, currentBlueStars
 
+// Score Visibility
+let isScoreVisible
+
+// Current Score
+const redCurrentScoreEl = document.getElementById("redCurrentScore")
+const redCurrentUnderlineEl = document.getElementById("redCurrentUnderline")
+const blueCurrentScoreEl = document.getElementById("blueCurrentScore")
+const blueCurrentUnderlineEl = document.getElementById("blueCurrentUnderline")
+const scoreDifferenceNumberEl = document.getElementById("scoreDifferenceNumber")
+const scoreDifferenceTextEl = document.getElementById("scoreDifferenceText")
+let currentScoreRed, currentScoreBlue, currentScoreDelta
+const scoreAnimation = {
+    redCurrentScore: new CountUp(redCurrentScoreEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ", ", decimal: "." }),
+    blueCurrentScore: new CountUp(blueCurrentScoreEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ", ", decimal: "." }),
+    scoreDifferenceNumber: new CountUp(scoreDifferenceNumberEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ", ", decimal: "." })
+}
+
+// RGBs
+const redTeamRedColourInverse = 255 - 244
+const redTeamGreenColourInverse = 255 - 98
+const redTeamBlueColourInverse = 255 - 98
+const blueTeamRedColourInverse = 255 - 99
+const blueTeamGreenColourInverse = 255 - 121
+const blueTeamBlueColourInverse = 255 - 219
+
 socket.onmessage = async (event) => {
     const data = JSON.parse(event.data)
 
+    // TODO: Flags
     // Team Name
     if (currentRedTeamName !== data.tourney.manager.teamName.left) {
         currentRedTeamName = data.tourney.manager.teamName.left
@@ -76,6 +102,56 @@ socket.onmessage = async (event) => {
         }
         for (i; i < currentFirstTo; i++) {
             blueTeamStarsEl.append(createStar("blueTeamStarContainer", i, "white_star"))
+        }
+    }
+
+    // Score Visibility
+    if (isScoreVisible !== data.tourney.manager.bools.scoreVisible) {
+        isScoreVisible = data.tourney.manager.bools.scoreVisible
+
+        if (isScoreVisible) {
+            redCurrentScoreEl.style.display = "block"
+            redCurrentUnderlineEl.style.display = "block"
+            blueCurrentScoreEl.style.display = "block"
+            blueCurrentUnderlineEl.style.display = "block"
+            scoreDifferenceNumberEl.style.display = "block"
+            scoreDifferenceTextEl.style.display = "block"
+        } else {
+            redCurrentScoreEl.style.display = "none"
+            redCurrentUnderlineEl.style.display = "none"
+            blueCurrentScoreEl.style.display = "none"
+            blueCurrentUnderlineEl.style.display = "none"
+            scoreDifferenceNumberEl.style.display = "none"
+            scoreDifferenceTextEl.style.display = "none"
+        }
+    }
+
+    // Score is showing
+    if (isScoreVisible) {
+        // Setting scores
+        currentScoreRed = data.tourney.manager.gameplay.score.left
+        currentScoreBlue = data.tourney.manager.gameplay.score.right
+        currentScoreDelta = Math.abs(currentScoreRed - currentScoreBlue)
+
+        // Score animation
+        scoreAnimation.redCurrentScore.update(currentScoreRed)
+        scoreAnimation.blueCurrentScore.update(currentScoreBlue)
+        scoreAnimation.scoreDifferenceNumber.update(currentScoreDelta)
+
+        // Score underline and score difference colour change
+        const currentDifference = Math.min(1000000, 1000000 - currentScoreDelta) / 1000000
+        if (currentScoreRed > currentScoreBlue) {
+            redCurrentUnderlineEl.style.opacity = 1
+            blueCurrentUnderlineEl.style.opacity = 0
+            scoreDifferenceNumberEl.style.color = `rgb(${255 - currentDifference * redTeamRedColourInverse}, ${255 - currentDifference * redTeamGreenColourInverse}, ${255 - currentDifference * redTeamBlueColourInverse})`
+        } else if (currentScoreRed === currentScoreBlue) {
+            redCurrentUnderlineEl.style.opacity = 0
+            blueCurrentUnderlineEl.style.opacity = 0
+            scoreDifferenceNumberEl.style.color = `rgb(255, 255, 255)`
+        } else if (currentScoreRed < currentScoreBlue) {
+            redCurrentUnderlineEl.style.opacity = 0
+            blueCurrentUnderlineEl.style.opacity = 1
+            scoreDifferenceNumberEl.style.color = `rgb(${255 - currentDifference * blueTeamRedColourInverse}, ${255 - currentDifference * blueTeamGreenColourInverse}, ${255 - currentDifference * blueTeamBlueColourInverse})`
         }
     }
 }
